48d1cfbf06193004aa2f6263cd63365cb0abcaa3
Marco Weiss <marco.a.weiss@gmail.com>
2019-07-24 15:12:22 +0200

    Fixed tab error

--- data/OxfordRobotcarDataset.py
+++ data/OxfordRobotcarDataset.py
@@ -63,7 +63,7 @@ class OxfordRobotcarDataset(Dataset):
     def __getitem__(self,idx):        
         img = self._get_anchor(idx)
         pcl = self._get_positive(idx,self.cache_pcl)
-	neg = None
+        neg = None
         if self.tuple_type=='triplet':
             neg = self._get_negative(idx)
         
