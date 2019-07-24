6a95ad4e36c96f678893e2b3fcb5dd56c469898d
Marco Weiss <marco.a.weiss@gmail.com>
2019-07-24 16:10:41 +0200

    Bugfix

--- data/OxfordRobotcarDataset.py
+++ data/OxfordRobotcarDataset.py
@@ -112,10 +112,8 @@ class OxfordRobotcarDataset(Dataset):
             pcl = torch.from_numpy(pcl).to(self.device)
             return pcl
                 
-    def _get_negative(self,idx, d_min=50.0):
+    def _get_negative(self,idx_anchor, d_min=50.0):
         # Find most similar pcl descriptor indices
-        idx_anchors = 
-        
         desc_anchor = self.img_descs[idx_anchor]
         k_max = int(2*d_min) + 2 # make sure there are at least 2 descriptors not within d_min
         indices_sim = self.kd_tree.query(desc_anchor.reshape(1, -1), k=k_max , sort_results=True, return_distance=False)
